5532bb2e54bcdfc0a605a16f987e53d0e1f43256
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2022-01-10 23:07:58 +0100

    changed the path to the icon

--- src/MainPage/AboutMe/styled.js
+++ src/MainPage/AboutMe/styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { ReactComponent as checked } from "../../images/icons/checked.svg";
+import { ReactComponent as checked } from "./images/icons/checked.svg";
 
 export const Wrapper = styled.section`
     display: grid;
