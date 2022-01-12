a5e1f5e96ccd2b7531722726b6749c89538fc26e
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2022-01-12 19:46:17 +0100

    added Skills and LearnNext

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -1,12 +1,17 @@
 import { Container } from "./Container";
 import AboutMe from "./AboutMe";
-import {ThemeSwitch} from "./ThemeSwitch";
+import { ThemeSwitch } from "./ThemeSwitch";
+import { Skills } from "./Skills";
+import { LearnNext } from "./LearnNext";
 
 const MainPage = () => (
-    <Container>
-        <ThemeSwitch />
-        <AboutMe />
-    </Container>
+  <Container>
+    <ThemeSwitch />
+    <AboutMe />
+    <Skills />
+    <LearnNext />
+
+  </Container>
 );
 
 export default MainPage;
