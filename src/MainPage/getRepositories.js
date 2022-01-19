1f5f31e5a88e303435e9939eba2791b2bbf2823a
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2022-01-19 21:45:25 +0100

    getting repositories from gitHub

---
+++ src/MainPage/getRepositories.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const getRepositories = async () => {
+  const response = await axios.get("https://api.github.com/users/Vov4ukz53/repos");
+
+  return await response.data;
+};
+
