89362d2104f48924f037cbdc2d54e68e08529728
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2022-01-19 21:48:58 +0100

    added LinkButton, info and wrapper style

---
+++ src/MainPage/Portfolio/info.js
@@ -0,0 +1,19 @@
+import styled, { css } from "styled-components";
+
+export const Info = styled.p`
+    font-size: 20px;
+    margin-bottom: 48px;
+    color: ${({ theme }) => theme.colors.title};
+
+    @media(max-width:${({ theme }) => theme.breakpoints.container}px){
+        font-size: calc(14px + (20 - 14) * ((100vw - 320px) / (1216 - 320)));
+        margin-bottom: calc(15px + (48 - 15) * ((100vw - 320px) / (1216 - 320)));
+    }
+
+    ${({ error }) => error && css`
+        max-width: 410px;
+        margin-bottom: 32px;
+        margin: 0 auto;
+        margin-bottom: 32px;
+    `}
+`;
---
+++ src/MainPage/linkButton.js
@@ -0,0 +1,40 @@
+import styled, { css } from "styled-components";
+
+export const LinkButton = styled.a`
+    width: 154px;
+    padding: 11px 0px;
+    font-size: 20px;
+    font-weight: 600;
+    letter-spacing: 0.05em;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: ${({ theme }) => theme.colors.textButton};
+    background: ${({ theme }) => theme.colors.backgroundButton};
+    transition: all .3s;
+    border: 1px solid ${({ theme }) => theme.colors.borderButton};
+    border-radius: 4px;
+    cursor: pointer;
+
+    ${({gitHub})=>gitHub && css`
+        margin: 0 auto;
+    `}
+
+    &:hover{
+        box-shadow:
+        2px -2px 0px ${({ theme }) => theme.colors.onHoverButton},
+        -2px 2px 0px ${({ theme }) => theme.colors.onHoverButton},
+        2px 2px 0px ${({ theme }) => theme.colors.onHoverButton},
+        -2px -2px 0px ${({ theme }) => theme.colors.onHoverButton};
+    }
+
+    &:active{
+        box-shadow: inset 0px 2px 0px ${({ theme }) => theme.colors.selectedButton};
+    }
+
+    @media(max-width:${({ theme }) => theme.breakpoints.container}px){
+        font-size: calc(18px + (20 - 18) * ((100vw - 320px) / (1216 - 320)));
+        width: calc(138px + (154 - 138) * ((100vw - 320px) / (1216 - 320)));
+    }
+`;
