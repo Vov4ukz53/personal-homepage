8284cef4d79ed63146c6e657a9794aa21ca5230c
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-07-04 21:08:43 +0200

    transfered files to core folder

--- src/getRepositories.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const getRepositories = async () => {
-  const response = await axios.get("https://api.github.com/users/Vov4ukz53/repos");
-
-  return response.data;
-};
-
