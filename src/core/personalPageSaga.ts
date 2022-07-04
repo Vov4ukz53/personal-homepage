e1de8f12878a78edcc6e74b5f508e297a311d3d9
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-07-04 21:09:19 +0200

    transfered files to core folder

---
+++ src/core/personalPageSaga.ts
@@ -0,0 +1,25 @@
+import { call, put, delay, takeLatest } from "redux-saga/effects";
+import { getRepositories } from "./getRepositories";
+import {
+  fetchRepositories,
+  setRepositories,
+  repositoriesRequestFailed
+} from "./personalPageSlice";
+
+function* fetchRepositoriesHandler() {
+  try {
+    yield delay(1000);
+    const repositories = yield call(getRepositories);
+    yield put(setRepositories(repositories));
+  }
+  catch (error) {
+    yield put(repositoriesRequestFailed());
+    console.error(error);
+  }
+};
+
+function* personalPageSaga() {
+  yield takeLatest(fetchRepositories.type, fetchRepositoriesHandler);
+};
+
+export default personalPageSaga;
