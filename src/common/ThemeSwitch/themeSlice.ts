4b8ee6b3889ac5372a780efff0fca05d5dafe2a1
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-07-06 17:30:43 +0200

    added type to states

--- src/common/ThemeSwitch/themeSlice.ts
+++ src/common/ThemeSwitch/themeSlice.ts
@@ -1,10 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../core/App/store";
+
+interface ThemeState {
+  isDarkTheme: boolean;
+};
+
+const initialState: ThemeState = {
+  isDarkTheme: false,
+};
 
 const themeSlice = createSlice({
   name: 'theme',
-  initialState: {
-    isDarkTheme: false,
-  },
+  initialState,
   reducers: {
     toggleTheme: state => {
       state.isDarkTheme = !state.isDarkTheme;
@@ -16,8 +23,8 @@ export const {
   toggleTheme
 } = themeSlice.actions;
 
-const selectThemeState = state => state.theme;
+const selectThemeState = (state: RootState) => state.theme;
 
-export const selectIsDarkTheme = state => selectThemeState(state).isDarkTheme;
+export const selectIsDarkTheme = (state: RootState) => selectThemeState(state).isDarkTheme;
 
 export default themeSlice.reducer;
--- src/core/personalPageSlice.ts
+++ src/core/personalPageSlice.ts
@@ -1,18 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./App/store";
+
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+};
+
+interface PersoalPageState {
+  repositories: Array<Repository>;
+  loading: boolean;
+  error: boolean;
+};
+
+const initialState: PersoalPageState = {
+  repositories: [],
+  loading: false,
+  error: false,
+};
 
 const personalPageSlice = createSlice({
   name: "repositories",
-  initialState: {
-    repositories: [],
-    loading: false,
-    error: false,
-  },
+  initialState,
   reducers: {
     fetchRepositories: state => {
       state.loading = !state.loading;
     },
-    setRepositories: (state, { payload: gitHubRepositories }) => {
-      state.repositories = gitHubRepositories;
+    setRepositories: (state, action: PayloadAction<[]>) => {
+      state.repositories = action.payload;
       state.loading = false;
     },
     repositoriesRequestFailed: state => {
@@ -28,13 +45,13 @@ export const {
   repositoriesRequestFailed,
 } = personalPageSlice.actions;
 
-const selectMainPageState = state => state.repositories;
+const selectMainPageState = (state: RootState) => state.repositories;
 
-export const selectRepositories = state => selectMainPageState(state).repositories;
-export const selectLoading = state => selectMainPageState(state).loading;
-export const selectError = state => selectMainPageState(state).error;
+export const selectRepositories = (state: RootState) => selectMainPageState(state).repositories;
+export const selectLoading = (state: RootState) => selectMainPageState(state).loading;
+export const selectError = (state: RootState) => selectMainPageState(state).error;
 
-export const selectMyRepositories = state => {
+export const selectMyRepositories = (state: RootState) => {
   const myRepositoriesIds = [
     424564004,
     484464982,
