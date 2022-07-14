90939efb2e411200d31b9c160656d1414a7de542
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-07-14 20:54:05 +0200

    added type to response

--- src/core/getRepositories.ts
+++ src/core/getRepositories.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { Repository } from "./personalPageSlice";
 
 export const getRepositories = async () => {
-  const response = await axios.get("https://api.github.com/users/Vov4ukz53/repos");
+  const response = await axios.get<Repository[]>("https://api.github.com/users/Vov4ukz53/repos");
 
   return response.data;
 };
