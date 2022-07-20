17baa53539bb63afa0160c992e91e5d863382fea
Vov4ukz53 <81194780+Vov4ukz53@users.noreply.github.com>
2022-07-20 18:29:04 +0200

    created colors and breakpoints interfaces

--- src/core/App/styled.d.ts
+++ src/core/App/styled.d.ts
@@ -1,31 +1,35 @@
 import "styled-components";
 
 declare module "styled-components" {
+
+  interface IBreakpoints {
+    container: number,
+    tablets: number,
+    mobile: number,
+    small: number,
+  }
+
+  interface IColors {
+    background: string,
+    backgroundTile: string,
+    borderTile: string,
+    hoverBorderTile: string,
+    shadowTile: string,
+    backgroundSwitch: string,
+    mainText: string,
+    title: string,
+    backgroundButton: string,
+    borderButton: string,
+    textButton: string,
+    onHoverButton: string,
+    focusButton: string,
+    selectedButton: string,
+    borderColor: string,
+    nameRepository: string,
+    icon: string,
+  }
   export interface DefaultTheme {
-    breakpoints: {
-      container: number,
-      tablets: number,
-      mobile: number,
-      small: number,
-    },
-    colors: {
-      background: string,
-      backgroundTile: string,
-      borderTile: string,
-      hoverBorderTile: string,
-      shadowTile: string,
-      backgroundSwitch: string,
-      mainText: string,
-      title: string,
-      backgroundButton: string,
-      borderButton: string,
-      textButton: string,
-      onHoverButton: string,
-      focusButton: string,
-      selectedButton: string,
-      borderColor: string,
-      nameRepository: string,
-      icon: string,
-    }
+    breakpoints: IBreakpoints,
+    colors: IColors,
   }
 }
